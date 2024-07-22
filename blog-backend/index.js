44f185f15d72dc54b1b91b24876d173c5608aa9e
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2024-07-22 21:32:46 +0300

    push project

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -15,19 +15,9 @@ const User = require("./models/user")(sequelize, DataTypes);
 const Post = require("./models/post")(sequelize, DataTypes);
 const Comment = require("./models/comment")(sequelize, DataTypes);
 
-// Associations
-// User.hasMany(Post);
-// Post.belongsTo(User);
-
-// User.hasMany(Comment);
-// Comment.belongsTo(User);
-
-// Post.hasMany(Comment);
-// Comment.belongsTo(Post);
 User.associate({ Post, Comment });
 Post.associate({ User, Comment });
 Comment.associate({ User, Post });
-// Sync models with database
 
 require("./startup/routes")(app);
 sequelize
